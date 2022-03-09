9e8b710a7f060ec795a44943774f41a77781f157
MarcZhu <a0983204766@gmail.com>
2022-03-09 14:22:49 +0800

    write

--- products.py
+++ products.py
@@ -11,3 +11,8 @@ print(products)
 for p in products:
 	print(p[0],p[1])
 
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	# f.write('商品' + ',' + '價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
